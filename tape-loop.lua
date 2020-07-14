d6a12113ad25dd23839aa5b3e3264222712bc577
RVRX <colemanning@yandex.com>
2020-07-14 17:28:34 -0400

    Update tape-loop.lua

--- tape-loop.lua
+++ tape-loop.lua
@@ -27,8 +27,11 @@ local function seekNCheckMultiple()
 	return true
 end
 	
-
+-- this could be made into a more efficient algo?
 local function findTapeEnd( ... )
+
+	local accuracy = 10
+	print("Using accuracy of " .. accuracy)
 
 	local tapeSize = tape.getSize()
 	tape.seek(-tapeSize) -- rewind tape
@@ -40,9 +43,9 @@ local function findTapeEnd( ... )
 		os.pullEvent()				 -- prevention
 
 
-		tape.seek(100) --seek forward one unit
+		tape.seek(accuracy) --seek forward one unit (One takes too long, bigger values not as accurate)
 		if tape.read() ~= 0 then --if current location is not a zero
-			runningEnd = i --Update Running runningEnd var
+			runningEnd = i*accuracy --Update Running runningEnd var. i * accuracy gets current location in tape
 			print("End Candidate: " .. runningEnd)
 		elseif seekNCheckMultiple() then --check a few spots away to see if zero as well
 			return runningEnd
@@ -52,5 +55,35 @@ local function findTapeEnd( ... )
 
 end
 
+--Main Function
+local function looper( ... )
+	print("Initializing...")
+	--find tape end
+	print("Locating end of song...")
+	local endLoc = findTapeEnd()
+	print("End of song at position " .. endLoc .. ", or " .. endLoc/6000 .. " seconds in\n")
 
-print(findTapeEnd())
+	print("Starting Loop! Hold Ctrl+T to Terminate")
+	while true do
+		tape.seek(-tape.getSize())
+		tape.play()
+		sleep(endLoc/6000)
+	end
+
+	--play tape until 
+end
+
+
+--[[
+
+A 4 minute tape is 1,440,000 units
+240 seconds is 1,440,000
+
+A 6 minute tape is 2,160,000 units
+360 seconds is 2,160,000 units
+
+Therefore, 1 second is 6000 UNITS
+
+add a config file for know cassettes
+
+]]
